6ff52a4da7f3cb5cce0e7bcf04358bec3a01b323
Gouxin <1019884428@qq.com>
2018-02-26 15:25:29 +0800

    update

--- e3-content/e3-content-service/src/main/java/cn/e3/content/service/impl/ContentServiceImpl.java
+++ e3-content/e3-content-service/src/main/java/cn/e3/content/service/impl/ContentServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -12,12 +13,14 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 
 import cn.e3.content.service.ContentService;
+import cn.e3.jedis.dao.JedisDao;
 import cn.e3.mapper.TbContentMapper;
 import cn.e3.pojo.TbContent;
 import cn.e3.pojo.TbContentExample;
 import cn.e3.pojo.TbContentExample.Criteria;
 import cn.e3.utils.AdItem;
 import cn.e3.utils.E3mallResult;
+import cn.e3.utils.JsonUtils;
 import cn.e3.utils.PageResult;
 
 @Service
@@ -87,14 +90,46 @@ public class ContentServiceImpl implements ContentService {
 	@Value("${HEIGHTB}")
 	private Integer HEIGHTB;
 	
+	//注入jedis对象
+	@Autowired
+	private JedisDao jedisDao;
+	
+	//注入首页缓存唯一标识
+	@Value("${INDEX_CACHE}")
+	private String INDEX_CACHE;
+	
 	/**
 	 * 需求:通过categoryId查询广告信息
 	 * 参数:Long categoryId
 	 * 返回值:List<AdItem>
-	 * @param categoryId
-	 * @return
+	 * 门户系统广告数据从数据库查询的,广告数据需要被频繁的读取,对数据库造成很大压力
+	 * 为了减轻数据库压力,添加redis缓存:
+	 * 添加缓存流程:
+	 * 1,先查询缓存,如果缓存中有数据,直接返回,不再查询数据库
+	 * 2,如果缓存没有数据,再查询数据库,同时把数据放入redis缓存
+	 * 操作redis数据库,使用redis提供客户端:jedis(set,get,hset,hget)
+	 * redis服务器存储缓存数据数据结构:
+	 * hash数据结构存储缓存数据:
+	 * key:INDEX_CACHE(首页缓存) FOOD_CACHE(食品页面缓存)
+	 * field:categoryId (区分区域缓存)
+	 * value:缓存值(json字符串)
 	 */
 	public List<AdItem> findContentByCategoryId(Long categoryId) {
+		
+		try {
+			//先查询缓存
+			String adJson = jedisDao.hget(INDEX_CACHE, categoryId+"");
+			//判断redis服务器缓存是否存在
+			if(StringUtils.isNotBlank(adJson)){
+				//缓存存在
+				List<AdItem> adList = JsonUtils.jsonToList(adJson, AdItem.class);
+				System.out.println("通过Redis查询获取");
+				return adList;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();  
+		}
+		
 		//创建门户系统展示广告信息集合
 		List<AdItem> AdList = new ArrayList<>();
 		TbContentExample example = new TbContentExample();
@@ -118,6 +153,9 @@ public class ContentServiceImpl implements ContentService {
 			adItem.setSrcB(tbContent.getPic2());
 			AdList.add(adItem);
 		}
+		//把数据库数据放入缓存
+		jedisDao.hset(INDEX_CACHE, categoryId+"", JsonUtils.objectToJson(AdList));
+		System.out.println("通过数据库查询获取");
 		return AdList;
 	}
 }
